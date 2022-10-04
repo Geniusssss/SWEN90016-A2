c5aa3249442a1e25520b392725378c3cddaa9a6d
MikJorden <616646638@qq.com>
2022-10-04 15:53:19 +1100

    Create button "Login as Admin" button on Welcome page

--- A2/src/views/WelcomePage.vue
+++ A2/src/views/WelcomePage.vue
@@ -5,6 +5,7 @@
         <el-container class="buttons">
             <el-button class="button" @click="routerTo('/login')">Login</el-button>
             <el-button class="button" @click="routerTo('/register')">Register</el-button>
+            <el-button class="button" @click="routerTo('/admin-login')">Login as Admin</el-button>
         </el-container>
     </el-container>
 </template>
